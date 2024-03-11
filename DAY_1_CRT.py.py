2ac2582ee3153027a219bc3160bdb7dc0c7ed31d
Adarshakuthala <162981769+Adarshakuthala@users.noreply.github.com>
2024-03-11 16:14:02 +0530

    Add files via upload

---
+++ DAY_1_CRT.py.py
@@ -0,0 +1,56 @@
+'''
+THEORY
+
+Day-1
+
+--->BARGAINED CHARACTERS
+>cotrols and Data structurs-> ABC launguage
+>strings and it formats-> From C
+>regular expressions->Pearl
+Exception Handling-> Module 3
+Magic Methods from-> Fortron
+
+
+--->WHAT KIND OF LAUNGAUGE IS PYTHON
+>Object oreinted programming laungauge
+>dynamically type laungauge
+>Interpreted laungauge
+>Compiled laungauge
+>Functional programmimg laungauge
+
+
+-->Flavours of Python
+>Cpython- Written in C, Most comm0on implementation of Python
+>Jython- Java implementation of Python
+>IronPython- Implementation of the Python programming laungauge which is thightly
+ integreted with.NET.
+>Brython - Browser Python, runs in browsers
+>RubyPython- bridge between Python and Ruby Interpreters
+>MicroPython - runs in Micro Controllers
+
+
+#print("Hellow")
+print("Hellow World")
+print("Hellow World")
+print("Hellow World")
+print("Hellow World")
+print("Hellow World")
+print("Hellow World")
+-----> Commenting
+1.)Commenting is used for making decomentation in a python file
+2.)when the line is comented, that line will not be considered as a part of the code.
+3.)when the program is excuted, commented line will not be in the part of excution.
+
+------> NOTE:
+Single line comenting can be done using # at the begining of the line
+    EX:
+
+            #print("helllo")
+----> import
+import this
+
+# import is a keyword which is used to communicated between the python files.add()
+'''
+import antigravity
+
+
